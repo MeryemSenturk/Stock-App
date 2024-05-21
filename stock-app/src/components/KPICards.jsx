4d9adb8416e8dcab1110f3973a3bb204137fc10c
meryem <meryemaydmr41@gmail.com>
2024-05-21 16:48:50 +0300

    kpıCards

--- stock-app/src/components/KPICards.jsx
+++ stock-app/src/components/KPICards.jsx
@@ -3,9 +3,10 @@ import Paper from "@mui/material/Paper";
 import Stack from "@mui/material/Stack";
 import Box from "@mui/material/Box";
 import Avatar from "@mui/material/Avatar";
-import { deepOrange} from "@mui/material/colors";
-import EuroIcon from '@mui/icons-material/Euro';
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { pink, deepPurple, amber } from "@mui/material/colors"
+import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
+import LocalMallIcon from "@mui/icons-material/LocalMall";
 import {Typography } from "@mui/material";
 
 const KPICards = () => {
@@ -13,28 +14,49 @@ const KPICards = () => {
       {
         id: 1,
         title: "Sales",
-        icon: <EuroIcon />,
+        icon: <MonetizationOnIcon sx={{ fontSize: "1.7rem" }} />,
         amount: "€12000",
-        color: "pink",
-        bgColor: "hotpink",
+        color: deepPurple[700],
+        bgColor: deepPurple[100],
       },
       {
         id: 2,
         title: "Profit",
-        icon: <ShoppingCartIcon />,
+        icon: <ShoppingBasketIcon sx={{ fontSize: "1.8rem" }} />,
         amount: "€35000",
-        color: "purple",
-        bgColor: "hotpink",
+        color: pink[700],
+        bgColor: pink[100],
+      },
+      {
+        id: 3,
+        title: "purchases",
+        icon: <LocalMallIcon sx={{ fontSize: "1.7rem" }} />,
+        amount: "€35000",
+        color: amber[700],
+        bgColor: amber[100],
       },
     ];
   return (
-    <Stack justifyContent={"center"} alignContent={"center"} flexWrap={"wrap"}>
+    <Stack justifyContent={"center"} alignItems={"center"} flexWrap={"wrap"} direction={"row"} gap={2}>
       {kpiData.map((data) => (
-        <Paper key={data.id} elevation={3}>
-          <Avatar sx={{ bgcolor: deepOrange[500] }}>N</Avatar>
+        <Paper
+          key={data.id}
+          elevation={3}
+          sx={{
+            display: "flex",
+            justifyContent: "flex-start",
+            width: 275,
+            p: 2,
+            pl: 3,
+            gap: 2,
+          }}
+        >
+          <Avatar sx={{ bgcolor: data.bgColor, color: data.color }}>
+            {data.icon}
+          </Avatar>
           <Box>
-            <Typography>{data.title}</Typography>
-            <Typography>{data.amount}</Typography>
+            <Typography variant="button">{data.title}</Typography>
+            <Typography variant="h5">{data.amount}</Typography>
           </Box>
         </Paper>
       ))}
