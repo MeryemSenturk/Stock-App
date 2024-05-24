4548aadbf1890a98fc6d31f11abca810bb189fea
meryem <meryemaydmr41@gmail.com>
2024-05-24 21:47:20 +0300

    chart01

--- stock-app/src/components/Charts.jsx
+++ stock-app/src/components/Charts.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import { AreaChart } from "@tremor/react";
+
+
+const dataFormatter = (number) =>
+  `$${Intl.NumberFormat("us").format(number).toString()}`;
 
 const Charts = () => {
-  return (
-    <div>Charts</div>
-  )
-}
 
-export default Charts
+  return (
+    <>
+      <AreaChart
+        className="h-80"
+        data={salesData}
+        index="date"
+        categories={["amount"]}
+        colors={["indigo"]}
+        valueFormatter={dataFormatter}
+        yAxisWidth={60}
+      />
+
+      <AreaChart
+        className="h-80"
+        data={purchasesData}
+        index="date"
+        categories={["amount"]}
+        colors={["red"]}
+        valueFormatter={dataFormatter}
+        yAxisWidth={60}
+      />
+    </>
+  );
+};
+export default Charts;
